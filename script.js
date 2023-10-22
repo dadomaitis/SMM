a278964e99ff6600c12bae1de32e1fa5ac66a463
dadomaitis <domasadomaitis@gmail.com>
2023-10-22 13:47:41 +0300

    gallery v1

---
+++ script.js
@@ -0,0 +1,18 @@
+// "use strict"
+
+// const images = document.getElementsByClassName("image")
+
+// for(var i = 0; i < images.length; i++){
+//     images[i].addEventListener("click", function(){
+//         this.classList.toggle("active");
+
+//         // // rmove active class
+//         // for(var image of images) {
+//         //     image.classList.remove("active")
+//         // }
+
+//         // // adds active class to link
+//         // image.classList.remove("content")
+//         // image.classList.toggle("active")
+//     })
+// }
